5ed833ea1d0f10ddc7f4df0974f229569a61ea84
diensa <diensa.refranto@gmail.com>
2012-10-30 01:15:58 +0700

    beta application
    beta test

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+		</div>
+		<div id="footer">Copyright 2012, Sonya Blog</div>
+	</body>
+</html>
+<?php
+	// 5. Close connection
+	mysql_close($connection);
+?>
---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Sonya Blog</title>
+		<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div id="header">
+			<h1>Sonya Blog</h1>
+		</div>
+		<div id="main">
